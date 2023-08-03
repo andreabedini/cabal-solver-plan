1b7a9d5dba38dacf2e157da7e7dc5e5139474a7c
Andrea Bedini <andrea@andreabedini.com>
2023-08-03 13:17:19 +0800

    Start moving options out

--- app/Main.hs
+++ app/Main.hs
@@ -12,38 +12,35 @@ import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
 import Data.Traversable (for)
-import Distribution.Client.Targets
-import Distribution.Client.Types.PackageLocation
-import Distribution.Compat.CharParsing qualified as P
+import Distribution.Client.Targets (UserConstraint)
+import Distribution.Client.Types.PackageLocation (UnresolvedSourcePackage)
 import Distribution.Compat.Graph (nodeKey)
 import Distribution.InstalledPackageInfo (parseInstalledPackageInfo)
-import Distribution.PackageDescription (PkgconfigVersion)
-import Distribution.Parsec (Parsec, eitherParsec, explicitEitherParsec, parsec)
-import Distribution.Simple
+import Distribution.Simple (PackageName, VersionRange, compilerInfo)
 import Distribution.Simple.GHC qualified as GHC
 import Distribution.Simple.PackageIndex (InstalledPackageIndex)
 import Distribution.Simple.PackageIndex qualified as Cabal.Index
-import Distribution.Simple.Program.Db
+import Distribution.Simple.Program.Db (defaultProgramDb)
 import Distribution.Simple.Utils (ordNubBy)
-import Distribution.Solver.Modular (PruneAfterFirstSuccess (PruneAfterFirstSuccess), SolverConfig (..))
+import Distribution.Solver.Modular (SolverConfig (..))
 import Distribution.Solver.Modular.Assignment (toCPs)
 import Distribution.Solver.Modular.ConfiguredConversion (convCP)
 import Distribution.Solver.Modular.Message (showMessages)
 import Distribution.Solver.Modular.RetryLog (toProgress)
-import Distribution.Solver.Types.ConstraintSource
-import Distribution.Solver.Types.PackageIndex as Solver.Index
-import Distribution.Solver.Types.PkgConfigDb
+import Distribution.Solver.Types.ConstraintSource (ConstraintSource (..))
+import Distribution.Solver.Types.PackageIndex qualified as Solver.Index
+import Distribution.Solver.Types.PkgConfigDb (PkgConfigDb (..), readPkgConfigDb)
 import Distribution.Solver.Types.Progress (foldProgress)
-import Distribution.Solver.Types.Settings
-import Distribution.System
+import Distribution.Solver.Types.Settings (PreferOldest (..))
+import Distribution.System (Platform (..), buildArch, buildOS)
 import Distribution.Types.Flag (FlagAssignment)
-import Distribution.Types.PackageVersionConstraint
+import Distribution.Types.PackageVersionConstraint (PackageVersionConstraint)
 import Distribution.Verbosity qualified as Verbosity
+import Options
 import Options.Applicative
 import Solver (compute)
-import System.Directory
+import System.Directory (listDirectory)
 import System.FilePath (takeExtension, (</>))
-import Text.Read (readMaybe)
 
 data Options = Options
   { compilerSource :: CompilerSource,
@@ -75,126 +72,17 @@ opts = info (s <**> helper) fullDesc
         <*> pure mempty -- FIXME: sourcePkgPrefs
         <*> (Set.fromList <$> many (parsecArgument (metavar "TARGET")))
 
-data CompilerSource
-  = CompilerInline CompilerInfo OS Arch [FilePath]
-  | CompilerFromCompiler (Maybe FilePath) PackageDBStack
-
-compilerSourceParser :: Parser CompilerSource
-compilerSourceParser =
-  asum
-    [ CompilerFromCompiler Nothing
-        <$ flag' () (long "with-system-ghc")
-        <*> many (option (parsePackageDb <$> str) (long "package-db" <> metavar "GLOBAL|USER|PATH")),
-      CompilerFromCompiler . Just
-        <$> strOption (long "with-ghc")
-        <*> many (option (parsePackageDb <$> str) (long "package-db" <> metavar "GLOBAL|USER|PATH")),
-      CompilerInline
-        <$> compilerInfoParser
-        <*> osParser
-        <*> archParser
-        <*> many (strOption (long "package-db" <> metavar "PATH"))
-    ]
-  where
-    compilerInfoParser =
-      CompilerInfo
-        <$> parsecOption (long "compiler-id")
-        <*> parsecOption (long "abi-tag" <> value NoAbiTag)
-        <*> optional (some (parsecOption (long "compiler-id-compat")))
-        <*> optional (some (parsecOption (long "language")))
-        <*> optional (some (parsecOption (long "extension")))
-    osParser = parsecOption (long "os" <> metavar "OS")
-    archParser = parsecOption (long "arch" <> metavar "ARCH")
-
-parsePackageDb :: String -> PackageDB
-parsePackageDb "global" = GlobalPackageDB
-parsePackageDb "user" = UserPackageDB
-parsePackageDb other = SpecificPackageDB other
-
--- data CompilerSource
---   = CompilerInline CompilerInfo OS Arch
---   | CompilerFromCompiler (Maybe FilePath)
---
--- compilerSourceParser :: Parser CompilerSource
--- compilerSourceParser =
---   asum
---     [ flag' (CompilerFromCompiler Nothing) (long "with-system-ghc"),
---       CompilerFromCompiler . Just <$> strOption (long "with-ghc"),
---       CompilerInline <$> compilerInfoParser <*> osParser <*> archParser
---     ]
---   where
---     compilerInfoParser =
---       CompilerInfo
---         <$> parsecOption (long "compiler-id")
---         <*> parsecOption (long "abi-tag" <> value NoAbiTag)
---         <*> optional (some (parsecOption (long "compiler-id-compat")))
---         <*> optional (some (parsecOption (long "language")))
---         <*> optional (some (parsecOption (long "extension")))
---     osParser = parsecOption (long "os" <> metavar "OS")
---     archParser = parsecOption (long "arch" <> metavar "ARCH")
-
-data PkgConfigDbSource
-  = PkgConfigDbEntry PkgconfigName (Maybe PkgconfigVersion)
-  | PkgConfigDbSystem
-
-pkgConfigDbSourceParser :: Parser PkgConfigDbSource
-pkgConfigDbSourceParser =
-  asum
-    [ option entryParser (long "pkgconfig-entry"),
-      flag' PkgConfigDbSystem (long "use-system-pkgconfig")
-    ]
-  where
-    entryParser = eitherReader $ explicitEitherParsec $ do
-      pkgName <- parsec
-      pkgVersion <- optional (P.string "=" *> parsec)
-      return $ PkgConfigDbEntry pkgName pkgVersion
-
-solverConfigParser :: Parser SolverConfig
-solverConfigParser =
-  SolverConfig
-    <$> (ReorderGoals <$> switch (long "reorder-goals"))
-    <*> (CountConflicts <$> switch (long "count-conflicts"))
-    <*> (FineGrainedConflicts <$> switch (long "fine-grained-conflicts"))
-    <*> (MinimizeConflictSet <$> switch (long "minimize-conflict-set"))
-    <*> (IndependentGoals <$> switch (long "independent-goals"))
-    <*> (AvoidReinstalls <$> switch (long "avoid-reinstalls"))
-    <*> (ShadowPkgs <$> switch (long "shadow-pkgs"))
-    <*> (StrongFlags <$> switch (long "strong-flags"))
-    <*> (AllowBootLibInstalls <$> switch (long "allow-boot-lib-installs"))
-    <*> option
-      (str >>= \case "none" -> pure OnlyConstrainedNone; "all" -> pure OnlyConstrainedAll; _ -> fail "bad only-constrainted")
-      (long "only-constrainted" <> value OnlyConstrainedNone)
-    <*> optional (option (maybeReader readMaybe) (long "max-backjumps"))
-    <*> (EnableBackjumping <$> switch (long "enable-backjumping"))
-    <*> (SolveExecutables <$> switch (long "solve-executables"))
-    <*> pure Nothing -- goalOrder
-    <*> parsecOption (long "verbosity" <> value Verbosity.normal)
-    <*> (PruneAfterFirstSuccess <$> switch (long "prune-after-first-success"))
-
-constraintsParser :: Parser (UserConstraint, ConstraintSource)
-constraintsParser =
-  (,ConstraintSourceCommandlineFlag) <$> parsecOption (long "constraint")
-
-flagAssignmentParser :: Parser (Map PackageName FlagAssignment)
-flagAssignmentParser =
-  Map.fromListWith (<>) <$> many (option (eitherReader $ explicitEitherParsec $ (,) <$> parsec <*> parsec) (long "flag"))
-
-parsecOption :: (Parsec a) => Mod OptionFields a -> Parser a
-parsecOption = option (eitherReader eitherParsec)
-
-parsecArgument :: (Parsec a) => Mod ArgumentFields a -> Parser a
-parsecArgument = argument (eitherReader eitherParsec)
-
 readPackageDb :: FilePath -> IO InstalledPackageIndex
 readPackageDb path = do
   filenames <- listDirectory path
-  ipis <- for (filter ((== ".conf") . takeExtension) filenames) $
-    \filename -> do
-      content <- BS.readFile (path </> filename)
-      case parseInstalledPackageInfo content of
-        Left e -> fail (unlines $ NE.toList e)
-        Right (warnings, ipi) -> do
-          for_ warnings print
-          return ipi
+  let packageDbEntryFiles = filter ((== ".conf") . takeExtension) filenames
+  ipis <- for packageDbEntryFiles $ \filename -> do
+    content <- BS.readFile (path </> filename)
+    case parseInstalledPackageInfo content of
+      Left e -> fail (unlines $ NE.toList e)
+      Right (warnings, ipi) -> do
+        for_ warnings print
+        return ipi
   return $ Cabal.Index.fromList ipis
 
 main :: IO ()
---
+++ app/Options.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Options where
+
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Distribution.Client.Targets (UserConstraint)
+import Distribution.Compat.CharParsing qualified as P
+import Distribution.PackageDescription (PkgconfigVersion, parsecFlagAssignmentNonEmpty)
+import Distribution.Parsec qualified as Parsec
+import Distribution.Simple (AbiTag (..), CompilerInfo (..), PackageDB (..), PackageDBStack, PackageName, PkgconfigName)
+import Distribution.Solver.Modular (PruneAfterFirstSuccess (..), SolverConfig (..))
+import Distribution.Solver.Types.ConstraintSource (ConstraintSource (..))
+import Distribution.Solver.Types.Settings
+  ( AllowBootLibInstalls (..),
+    AvoidReinstalls (..),
+    CountConflicts (..),
+    EnableBackjumping (..),
+    FineGrainedConflicts (..),
+    IndependentGoals (..),
+    MinimizeConflictSet (..),
+    OnlyConstrained (..),
+    ReorderGoals (..),
+    ShadowPkgs (..),
+    SolveExecutables (..),
+    StrongFlags (..),
+  )
+import Distribution.System (Arch, OS)
+import Distribution.Types.Flag (FlagAssignment)
+import Distribution.Verbosity qualified as Verbosity
+import Options.Applicative
+import Text.Read (readMaybe)
+
+data CompilerSource
+  = CompilerInline CompilerInfo OS Arch [FilePath]
+  | CompilerFromCompiler (Maybe FilePath) PackageDBStack
+
+compilerSourceParser :: Parser CompilerSource
+compilerSourceParser =
+  asum
+    [ CompilerFromCompiler Nothing
+        <$ flag' () (long "with-system-ghc")
+        <*> many (option (parsePackageDb <$> str) (long "package-db" <> metavar "GLOBAL|USER|PATH")),
+      CompilerFromCompiler . Just
+        <$> strOption (long "with-ghc")
+        <*> many (option (parsePackageDb <$> str) (long "package-db" <> metavar "GLOBAL|USER|PATH")),
+      CompilerInline
+        <$> compilerInfoParser
+        <*> osParser
+        <*> archParser
+        <*> many (strOption (long "package-db" <> metavar "PATH"))
+    ]
+  where
+    compilerInfoParser =
+      CompilerInfo
+        <$> parsecOption (long "compiler-id")
+        <*> parsecOption (long "abi-tag" <> value NoAbiTag)
+        <*> optional (some (parsecOption (long "compiler-id-compat")))
+        <*> optional (some (parsecOption (long "language")))
+        <*> optional (some (parsecOption (long "extension")))
+    osParser = parsecOption (long "os" <> metavar "OS")
+    archParser = parsecOption (long "arch" <> metavar "ARCH")
+
+parsePackageDb :: String -> PackageDB
+parsePackageDb "global" = GlobalPackageDB
+parsePackageDb "user" = UserPackageDB
+parsePackageDb other = SpecificPackageDB other
+
+data PkgConfigDbSource
+  = PkgConfigDbEntry PkgconfigName (Maybe PkgconfigVersion)
+  | PkgConfigDbSystem
+
+pkgConfigDbSourceParser :: Parser PkgConfigDbSource
+pkgConfigDbSourceParser =
+  asum
+    [ parsecOptionWith entryParser (long "pkgconfig-entry"),
+      flag' PkgConfigDbSystem (long "use-system-pkgconfig")
+    ]
+  where
+    entryParser = do
+      pkgName <- Parsec.parsec
+      pkgVersion <- optional (P.string "=" *> Parsec.parsec)
+      return $ PkgConfigDbEntry pkgName pkgVersion
+
+solverConfigParser :: Parser SolverConfig
+solverConfigParser =
+  SolverConfig
+    <$> (ReorderGoals <$> switch (long "reorder-goals"))
+    <*> (CountConflicts <$> switch (long "count-conflicts"))
+    <*> (FineGrainedConflicts <$> switch (long "fine-grained-conflicts"))
+    <*> (MinimizeConflictSet <$> switch (long "minimize-conflict-set"))
+    <*> (IndependentGoals <$> switch (long "independent-goals"))
+    <*> (AvoidReinstalls <$> switch (long "avoid-reinstalls"))
+    <*> (ShadowPkgs <$> switch (long "shadow-pkgs"))
+    <*> (StrongFlags <$> switch (long "strong-flags"))
+    <*> (AllowBootLibInstalls <$> switch (long "allow-boot-lib-installs"))
+    <*> onlyConstrainedParser
+    <*> optional (option (maybeReader readMaybe) (long "max-backjumps"))
+    <*> (EnableBackjumping <$> switch (long "enable-backjumping"))
+    <*> (SolveExecutables <$> switch (long "solve-executables"))
+    <*> pure Nothing -- goalOrder
+    <*> parsecOption (long "verbosity" <> value Verbosity.normal)
+    <*> (PruneAfterFirstSuccess <$> switch (long "prune-after-first-success"))
+
+onlyConstrainedParser :: Parser OnlyConstrained
+onlyConstrainedParser =
+  option reader (long "only-constrainted" <> value OnlyConstrainedNone)
+  where
+    reader =
+      str >>= \case
+        "none" -> pure OnlyConstrainedNone
+        "all" -> pure OnlyConstrainedAll
+        _ -> fail "bad only-constrainted"
+
+constraintsParser :: Parser (UserConstraint, ConstraintSource)
+constraintsParser =
+  (,ConstraintSourceCommandlineFlag) <$> parsecOption (long "constraint")
+
+flagAssignmentParser :: Parser (Map PackageName FlagAssignment)
+flagAssignmentParser =
+  Map.fromListWith (<>) <$> many (parsecOptionWith parser (long "flag"))
+  where
+    parser = (,) <$> Parsec.parsec <* P.char ':' <*> parsecFlagAssignmentNonEmpty
+
+parsecOption :: (Parsec.Parsec a) => Mod OptionFields a -> Parser a
+parsecOption = option (eitherReader Parsec.eitherParsec)
+
+parsecOptionWith :: Parsec.ParsecParser a -> Mod OptionFields a -> Parser a
+parsecOptionWith parser = option (eitherReader (Parsec.explicitEitherParsec parser))
+
+parsecArgument :: (Parsec.Parsec a) => Mod ArgumentFields a -> Parser a
+parsecArgument = argument (eitherReader Parsec.eitherParsec)
