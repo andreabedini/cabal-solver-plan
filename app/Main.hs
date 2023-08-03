abab5800feab61761997f8ae52d7bbe9c784701c
Andrea Bedini <andrea@andreabedini.com>
2023-08-03 15:06:29 +0800

    Add repositories

--- app/Main.hs
+++ app/Main.hs
@@ -12,11 +12,15 @@ import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
 import Data.Traversable (for)
+import Distribution.Client.GlobalFlags (RepoContext (..), withRepoContext')
+import Distribution.Client.IndexUtils (Index (..), getSourcePackagesAtIndexState, updateRepoIndexCache)
 import Distribution.Client.Targets (UserConstraint)
-import Distribution.Client.Types.PackageLocation (UnresolvedSourcePackage)
+import Distribution.Client.Types (RemoteRepo (..), Repo (..), RepoName (..), SourcePackageDb (..), emptyRemoteRepo)
 import Distribution.Compat.Graph (nodeKey)
 import Distribution.InstalledPackageInfo (parseInstalledPackageInfo)
-import Distribution.Simple (PackageName, VersionRange, compilerInfo)
+import Distribution.Parsec (simpleParsec)
+import Distribution.Pretty (prettyShow)
+import Distribution.Simple (PackageName, compilerInfo)
 import Distribution.Simple.GHC qualified as GHC
 import Distribution.Simple.PackageIndex (InstalledPackageIndex)
 import Distribution.Simple.PackageIndex qualified as Cabal.Index
@@ -28,7 +32,6 @@ import Distribution.Solver.Modular.ConfiguredConversion (convCP)
 import Distribution.Solver.Modular.Message (showMessages)
 import Distribution.Solver.Modular.RetryLog (toProgress)
 import Distribution.Solver.Types.ConstraintSource (ConstraintSource (..))
-import Distribution.Solver.Types.PackageIndex qualified as Solver.Index
 import Distribution.Solver.Types.PkgConfigDb (PkgConfigDb (..), readPkgConfigDb)
 import Distribution.Solver.Types.Progress (foldProgress)
 import Distribution.Solver.Types.Settings (PreferOldest (..))
@@ -36,23 +39,25 @@ import Distribution.System (Platform (..), buildArch, buildOS)
 import Distribution.Types.Flag (FlagAssignment)
 import Distribution.Types.PackageVersionConstraint (PackageVersionConstraint)
 import Distribution.Verbosity qualified as Verbosity
+import Hackage.Security.Client qualified as Sec
+import Network.URI (URI (..), URIAuth (..), parseAbsoluteURI)
 import Options
 import Options.Applicative
 import Solver (compute)
-import System.Directory (listDirectory)
-import System.FilePath (takeExtension, (</>))
+import System.Directory (createDirectoryIfMissing, listDirectory)
+import System.FilePath (takeBaseName, takeExtension, (</>))
 
 data Options = Options
   { compilerSource :: CompilerSource,
-    pkgConfigDbSource :: [PkgConfigDbSource],
+    pkgConfigDbSources :: [PkgConfigDbSource],
     constraints :: [(UserConstraint, ConstraintSource)],
     preferences :: [PackageVersionConstraint],
     flagAssignments :: Map PackageName FlagAssignment,
     solverConfig :: SolverConfig,
     preferOldest :: PreferOldest,
-    sourcePkgIndex :: Solver.Index.PackageIndex UnresolvedSourcePackage,
-    sourcePkgPrefs :: Map PackageName VersionRange,
-    targets :: Set PackageName
+    remoteRepos :: [RemoteRepo],
+    targets :: Set PackageName,
+    cacheDir :: FilePath
   }
 
 opts :: ParserInfo Options
@@ -68,9 +73,33 @@ opts = info (s <**> helper) fullDesc
         <*> flagAssignmentParser
         <*> solverConfigParser
         <*> (PreferOldest <$> switch (long "prefer-oldest"))
-        <*> pure mempty -- FIXME: sourcePkgIndex
-        <*> pure mempty -- FIXME: sourcePkgPrefs
+        <*> many repositoryParser
         <*> (Set.fromList <$> many (parsecArgument (metavar "TARGET")))
+        <*> strOption (long "cache-dir" <> value "_cache")
+
+-- NOTE: all this it to allow users to use a shorter form for reposiotries
+-- cabal requires repo-name:repo-uri but we try to make up a repo-name from its
+-- uri. Also not the clearest code.
+repositoryParser :: Parser RemoteRepo
+repositoryParser = option g (long "repository")
+  where
+    g =
+      makeSecure
+        <$> ( str
+                >>= \s ->
+                  maybe (fail "cannot parse repository") return ((parseAbsoluteURI s >>= f) <|> simpleParsec s)
+            )
+
+    f uri@(URI {uriScheme, uriPath})
+      | uriScheme == "file:" =
+          Just (emptyRemoteRepo (RepoName (takeBaseName uriPath))) {remoteRepoURI = uri}
+    f uri@(URI {uriScheme, uriPath, uriAuthority = Just (URIAuth {uriRegName})})
+      | null uriPath,
+        uriScheme `elem` ["http:", "https:"] =
+          Just (emptyRemoteRepo (RepoName uriRegName)) {remoteRepoURI = uri}
+    f _otherwise = Nothing
+
+    makeSecure repo = repo {remoteRepoSecure = Just True}
 
 readPackageDb :: FilePath -> IO InstalledPackageIndex
 readPackageDb path = do
@@ -89,12 +118,6 @@ main :: IO ()
 main = do
   Options {..} <- execParser opts
 
-  -- let sourcePackages :: Solver.PackageIndex.PackageIndex UnresolvedSourcePackage
-  --     sourcePackages =
-  --       removeLowerBounds solverSettingAllowOlder $
-  --         removeUpperBounds solverSettingAllowNewer $
-  --           applyDefaultSetupDeps compiler platform <$> sourcePkgIndex
-
   (cinfo, os, arch, installedPkgIndex) <-
     case compilerSource of
       CompilerInline cinfo os arch packagedbs -> do
@@ -107,7 +130,7 @@ main = do
         return (compilerInfo compiler, os, arch, installedPkgIndex)
 
   pkgConfigDb <-
-    case NE.nonEmpty pkgConfigDbSource of
+    case NE.nonEmpty pkgConfigDbSources of
       Nothing -> return NoPkgConfigDb
       Just sources ->
         PkgConfigDb . Map.fromList
@@ -120,6 +143,25 @@ main = do
                     NoPkgConfigDb -> mempty
             )
             sources
+
+  -- NOTE: This is workaround for a bug in cabal-install where it
+  -- tries to create hackage-security-lock in repoLocalDir, without
+  -- making sure that directory exists.
+  for_ remoteRepos $ \RemoteRepo {remoteRepoName} ->
+    createDirectoryIfMissing True (cacheDir </> unRepoName remoteRepoName)
+
+  (SourcePackageDb sourcePkgIndex sourcePkgPrefs, _tis, _ar) <-
+    withRepoContext' Verbosity.normal remoteRepos [] cacheDir Nothing (Just True) [] $ \repoContext@RepoContext {..} -> do
+      for_ repoContextRepos $ \case
+        repo@RepoSecure {repoRemote} -> do
+          repoContextWithSecureRepo repo $ \repoSecure -> do
+            updated <- Sec.uncheckClientErrors $ Sec.checkForUpdates repoSecure Nothing
+            case updated of
+              Sec.NoUpdates -> putStrLn $ "no updates for " ++ prettyShow repoRemote
+              Sec.HasUpdates -> updateRepoIndexCache Verbosity.normal (RepoIndex repoContext repo)
+        _otherwise -> pure ()
+
+      getSourcePackagesAtIndexState Verbosity.normal repoContext Nothing Nothing
 
   foldProgress
     -- step
